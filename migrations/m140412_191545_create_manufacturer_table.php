eff207810ae06f068254e00ee53da7ea8ca145d5
Mario Rossi <veontomo@gmail.com>
2014-04-19 20:55:39 +0200

    multi language Manufacturer is added

--- migrations/m140412_191545_create_manufacturer_table.php
+++ migrations/m140412_191545_create_manufacturer_table.php
@@ -4,59 +4,56 @@ use yii\db\Schema;
 
 class m140412_191545_create_manufacturer_table extends \yii\db\Migration
 {
-	private $tableName = 'teresa_manufacturer';
-	private $tableNameAttrs = 'teresa_manufacturer_LDA'; // table that contains names of language-dependent attributes
-	private $tableNameLD = 'teresa_manufacturer_LD';     // table that contains language-dependent data
-	private $tableLang = 'teresa_lang'; 
+	private $name   = 'teresa_manufacturer';   // core table name
+	private $attrs  = 'teresa_manufacturer_attrs';      // table name for language-dependent attributes
+	private $values = 'teresa_manufacturer_values';     // table name for language-dependent values
+	private $lang   = 'teresa_lang'; 	                // name of language table
 
-	private $fkAddedBy = 'manufacturerAddedBy';
-	private $fkUpdatedBy = 'manufacturerUpdatedBy';
-	private $fkLDAddedBy = 'manufacturerLDAddedBy';
-	private $fkLDUpdatedBy = 'manufacturerLDUpdatedBy';
-	private $fkLang = 'manufacturerLDlang';
-	private $fkAttr = 'manufacturerLDAttrs';
-	private $fkToMain = 'manufacturerLDToMain'; 
 
+	private $fkAddedBy = 'manufacturer_addedBy';
+	private $fkUpdatedBy = 'manufacturer_updatedBy';
+	private $fkValuesAddedBy = 'manufacturer_values_addedBy';
+	private $fkValuesUpdatedBy = 'manufacturer_values_updatedBy';
+	private $fkLang = 'manufacturer_values_lang';
+	private $fkAttr = 'manufacturer_values_attrs';
+	private $fkToCore = 'manufacturer_values_to_core'; 
 
 	public function up()
 	{
-		$this->createTable($this->tableName, [
-			'id' => 'INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
-			// 'fullName' => 'VARCHAR(150)',
-			// 'shortName' => 'VARCHAR(30)',
+		$this->createTable($this->name, [
+			'id'  => 'INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
 			'url' => 'VARCHAR(255)',
-			// 'description' => 'TEXT',
 			'addedBy' => 'SMALLINT(5) unsigned',
 			'creationTime' => 'DATETIME NOT NULL',
-			'updatedBy' => 'SMALLINT(5) unsigned DEFAULT NULL',
+			'updatedBy'  => 'SMALLINT(5) unsigned DEFAULT NULL',
 			'updateTime' => 'DATETIME DEFAULT NULL',
 		]);
-		echo "Table $this->tableName is created.\n";
-		$this->addForeignKey($this->fkAddedBy, $this->tableName, 'addedBy', 
+		echo "Table $this->name is created.\n";
+		$this->addForeignKey($this->fkAddedBy, $this->name, 'addedBy', 
 			'teresa_admin', 'id', 'restrict', 'cascade');
-		echo "Foreign key $this->fkAddedBy is added to $this->tableName.\n";
-		$this->addForeignKey($this->fkUpdatedBy, $this->tableName, 'updatedBy', 
+		echo "Foreign key $this->fkAddedBy is added to $this->name.\n";
+		$this->addForeignKey($this->fkUpdatedBy, $this->name, 'updatedBy', 
 			'teresa_admin', 'id', 'restrict', 'cascade');
-		echo "Foreign key $this->fkUpdatedBy is added to $this->tableName.\n";
+		echo "Foreign key $this->fkUpdatedBy is added to $this->name.\n";
 
 
 		// table that stores the names of the attributes that depend on language
-		$this->createTable($this->tableNameAttrs, [
+		$this->createTable($this->attrs, [
 			'attribute' => 'VARCHAR(20) NOT NULL PRIMARY KEY'
 		]);
-		echo "Table $this->tableNameAttrs is created.\n";
+		echo "Table $this->attrs is created.\n";
 		// populating the table with attributes which values depend on language 
-		$this->insert($this->tableNameAttrs, ['attribute' => 'fullName']);
-		echo "\"fullName\" is inserted in $this->tableName.\n";
-		$this->insert($this->tableNameAttrs, ['attribute' => 'shortName']);
-		echo "\"shortName\" is inserted in $this->tableName.\n";
-		$this->insert($this->tableNameAttrs, ['attribute' => 'description']);
-		echo "\"description\" is inserted in $this->tableName.\n";
+		$this->insert($this->attrs, ['attribute' => 'fullName']);
+		echo "\"fullName\" is inserted in $this->attrs.\n";
+		$this->insert($this->attrs, ['attribute' => 'shortName']);
+		echo "\"shortName\" is inserted in $this->attrs.\n";
+		$this->insert($this->attrs, ['attribute' => 'description']);
+		echo "\"description\" is inserted in $this->attrs.\n";
 
 		// creating table that stores language-dependent values 
-		$this->createTable($this->tableNameLD, [
-			'id' => 'INT(6) UNSIGNED NOT NULL', // bound to "id" of language-independent table record
-			'attribute' => 'VARCHAR(20)',            // bound to "attributes" of table with attribute names
+		$this->createTable($this->values, [
+			'id' => 'INT(6) UNSIGNED NOT NULL',   // bound to "id" of the core table record
+			'attribute' => 'VARCHAR(20)',         // bound to "attribute" of the attribute table names
 			'lang' => 'INT(3) UNSIGNED NOT NULL', // bound to values from language table
 			'value' => 'TEXT',
 			'addedBy' => 'SMALLINT(5) unsigned',
@@ -65,65 +62,61 @@ class m140412_191545_create_manufacturer_table extends \yii\db\Migration
 			'updateTime' => 'DATETIME DEFAULT NULL',
 			'PRIMARY KEY (`id`, `attribute`, `lang`)'
 			]);
-		echo "Table $this->tableNameLD is created.\n";
+		echo "Table $this->values is created.\n";
 
-		$this->addForeignKey($this->fkLDAddedBy, $this->tableNameLD, 'addedBy', 
+		$this->addForeignKey($this->fkValuesAddedBy, $this->values, 'addedBy', 
 			'teresa_admin', 'id', 'restrict', 'cascade');
-		echo "Foreign key $this->fkAddedBy is added to $this->tableNameLD.\n";
+		echo "Foreign key $this->fkAddedBy is added to $this->values.\n";
 
-		$this->addForeignKey($this->fkLDUpdatedBy, $this->tableNameLD, 'updatedBy', 
+		$this->addForeignKey($this->fkValuesUpdatedBy, $this->values, 'updatedBy', 
 			'teresa_admin', 'id', 'restrict', 'cascade');
-		echo "Foreign key $this->fkUpdatedBy is added to $this->tableNameLD.\n";
+		echo "Foreign key $this->fkUpdatedBy is added to $this->values.\n";
 
-		$this->addForeignKey($this->fkLang, $this->tableNameLD, 'lang', 
-			'teresa_lang', 'id', 'restrict', 'cascade');
-		echo "Foreign key $this->fkLang is added to $this->tableNameLD.\n";
+		$this->addForeignKey($this->fkLang, $this->values, 'lang', 
+			$this->lang, 'id', 'restrict', 'cascade');
+		echo "Foreign key $this->fkLang is added to $this->values.\n";
 
-		$this->addForeignKey($this->fkAttr, $this->tableNameLD, 'attribute', 
-			$this->tableNameAttrs, 'attribute', 'restrict', 'cascade');
-		echo "Foreign key $this->fkAttr is added to $this->tableNameLD.\n";
+		$this->addForeignKey($this->fkAttr, $this->values, 'attribute', 
+			$this->attrs, 'attribute', 'restrict', 'cascade');
+		echo "Foreign key $this->fkAttr is added to $this->values.\n";
 
-		$this->addForeignKey($this->fkToMain, $this->tableNameLD, 'id', 
-			$this->tableName, 'id', 'restrict', 'cascade');
-		echo "Foreign key $this->fkToMain is added to $this->tableNameLD.\n";
+		$this->addForeignKey($this->fkToCore, $this->values, 'id', 
+			$this->name, 'id', 'restrict', 'cascade');
+		echo "Foreign key $this->fkToCore is added to $this->values.\n";
 	}
 
 	public function down()
 	{
-		$this->dropForeignKey($this->fkToMain, $this->tableNameLD);
-		echo "Foreign key $this->fkToMain is dropped from $this->tableNameLD.\n";
+		$this->dropForeignKey($this->fkToCore, $this->values);
+		echo "Foreign key $this->fkToCore is dropped from $this->values.\n";
 
-		$this->dropForeignKey($this->fkAttr, $this->tableNameLD);
-		echo "Foreign key $this->fkAttr is dropped from $this->tableNameLD.\n";
+		$this->dropForeignKey($this->fkAttr, $this->values);
+		echo "Foreign key $this->fkAttr is dropped from $this->values.\n";
 
-		$this->dropForeignKey($this->fkLang, $this->tableNameLD);
-		echo "Foreign key $this->fkLang is dropped from $this->tableNameLD.\n";
+		$this->dropForeignKey($this->fkLang, $this->values);
+		echo "Foreign key $this->fkLang is dropped from $this->values.\n";
 
-		$this->dropForeignKey($this->fkLDUpdatedBy, $this->tableNameLD);
-		echo "Foreign key $this->fkUpdatedBy is dropped from $this->tableName.\n";
+		$this->dropForeignKey($this->fkValuesUpdatedBy, $this->values);
+		echo "Foreign key $this->fkUpdatedBy is dropped from $this->values.\n";
 
-		$this->dropForeignKey($this->fkLDAddedBy, $this->tableNameLD);
-		echo "Foreign key $this->fkAddedBy is dropped from $this->tableName.\n";
+		$this->dropForeignKey($this->fkValuesAddedBy, $this->values);
+		echo "Foreign key $this->fkAddedBy is dropped from $this->values.\n";
 
+		$this->dropTable($this->values);
+		echo "Table  $this->values is dropped.\n";
 
-		$this->dropTable($this->tableNameLD);
-		echo "Table  $this->tableNameLD is dropped.\n";
+		$this->dropTable($this->attrs);
+		echo "Table  $this->attrs is dropped.\n";
 
-		$this->dropTable($this->tableNameAttrs);
-		echo "Table  $this->tableNameAttrs is dropped.\n";
+		$this->dropForeignKey($this->fkUpdatedBy, $this->name);
+		echo "Foreign key $this->fkUpdatedBy is droped from $this->name.\n";
 
-		$this->dropForeignKey($this->fkUpdatedBy, $this->tableName);
-		echo "Foreign key $this->fkUpdatedBy is droped from $this->tableName.\n";
+		$this->dropForeignKey($this->fkAddedBy, $this->name);
+		echo "Foreign key $this->fkAddedBy is dropped from $this->name.\n";
 
-
-		$this->dropForeignKey($this->fkAddedBy, $this->tableName);
-		echo "Foreign key $this->fkAddedBy is dropped from $this->tableName.\n";
-
-
-		$this->dropTable($this->tableName);
-		echo "Table  $this->tableName is dropped.\n";
+		$this->dropTable($this->name);
+		echo "Table  $this->name is dropped.\n";
 
 		return true;
 	}
-
 }
